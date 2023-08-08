e9db852a5da3cd00bae76706f2437ba88a51c70d
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2023-08-08 15:09:01 +0200

    save design et mise en page

--- frontend/src/components/home/competence.jsx
+++ frontend/src/components/home/competence.jsx
@@ -12,21 +12,29 @@ function Competence({open}) {
                 <div>
                     <h3>&#123; Languages &#125;</h3>
                     <ul>
+                        <li>Html</li>
+                        <li>Css</li>
+                        <li>JavaScript</li>
 
                     </ul>
                 </div>
                 <div>
                     <h3>&#123; Outils &#125;</h3>
                     <ul>
-                        
+                        <li>Visual Studio Code</li>
+                        <li>React</li>
+                        <li>Sass</li>
+                        <li>Node</li>
+                        <li>MongoDb</li>
                     </ul>
                 </div>
-                <div>
+                {/* <div>
                     <h3>&#123; Hobbies &#125;</h3>
                     <ul>
-                        
+                        <li>Running</li>
+                        <li>Natation</li>
                     </ul>
-                </div>
+                </div> */}
             </div>
         </section>
     )
